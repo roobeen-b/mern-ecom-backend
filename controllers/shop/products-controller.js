3b73f706725f72ad7aed02bcd79a44431568206f
roobeen-b <rubinbaidhya@gmail.com>
2024-11-19 17:32:40 +0545

    Added API for fetching product details

--- controllers/shop/products-controller.js
+++ controllers/shop/products-controller.js
@@ -57,4 +57,29 @@ const fetchAllFilteredProducts = async (req, res) => {
   }
 };
 
-module.exports = { fetchAllFilteredProducts };
+const getProductDetails = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const foundProduct = await Product.findById(id);
+    if (!foundProduct) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: foundProduct,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
+    });
+  }
+};
+
+module.exports = { fetchAllFilteredProducts, getProductDetails };
--- routes/shop/products-routes.js
+++ routes/shop/products-routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {
   fetchAllFilteredProducts,
+  getProductDetails,
 } = require("../../controllers/shop/products-controller");
 
 const router = express.Router();
 
 router.get("/get", fetchAllFilteredProducts);
+router.get("/get/:id", getProductDetails);
 
 module.exports = router;
