d58b2876882b4bb1c34820f3ca84665e1e9f9d0a
roobeen-b <rubinbaidhya@gmail.com>
2024-11-27 01:02:42 +0545

    Added ap to upload feature image

--- server.js
+++ server.js
@@ -6,14 +6,18 @@ const cors = require("cors");
 require("dotenv").config();
 
 const authRouter = require("./routes/auth/auth-routes");
+
 const adminProductsRouter = require("./routes/admin/products-routes");
 const adminOrderRouter = require("./routes/admin/order-routes");
+
 const shopProductsRouter = require("./routes/shop/products-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
 const shopAddressRouter = require("./routes/shop/address-routes");
 const shopOrderRouter = require("./routes/shop/order-routes");
 const searchProductsRouter = require("./routes/shop/search-routes");
 const productReviewRouter = require("./routes/shop/product-review-routes");
+
+const commonFeatureRouter = require("./routes/common/feature-routes");
 
 mongoose
   .connect(
@@ -47,12 +51,15 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/admin/order", adminOrderRouter);
+
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", searchProductsRouter);
 app.use("/api/shop/review", productReviewRouter);
+
+app.use("/api/common/feature", commonFeatureRouter);
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
